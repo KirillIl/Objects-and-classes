2272fea4a0fa6eb596ff713cb9aac55f287b9d8d
Admin <ilinskiy_kirill@mail.ru>
2022-11-11 14:37:27 +0300

    исправлено, проверьте плиз, надеюсь все работает)

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,8 @@
 public class Book {
     private final String bookName;
-    private final String authorName;
+    private final Author authorName;
     private int publishingYear;
-    public Book(String bookName, String authorName, int publishingYear) {
+    public Book(String bookName, Author authorName, int publishingYear) {
         this.bookName = bookName;
         this.authorName = authorName;
         this.publishingYear = publishingYear;
@@ -10,7 +10,7 @@ public class Book {
     public String getBookName () {
         return bookName;
     }
-    public String getAuthorName () {
+    public Author getAuthorName () {
         return authorName;
     }
     public int getPublishingYear () {
